951cb5caf36d8e9a6c71e6b1f3ca4e0659b096b2
Sam <sammccathie2@gmail.com>
2022-04-12 11:40:26 +1200

    Complete forms

--- src/topics/forms/Forms.js
+++ src/topics/forms/Forms.js
@@ -8,8 +8,9 @@ const Forms = () => {
     about: "",
     friendly: true,
     employment: "",
+    faveColour: "",
   });
-  const {first, last, about, friendly, employment} = formData;
+  const {first, last, about, friendly, employment, faveColour} = formData;
 
   const handleChange = (event) => {
     const {name, value, type, checked} = event.target;
@@ -22,13 +23,20 @@ const Forms = () => {
     });
   };
 
-  console.log(formData);
+  const handleSubmit = (event) => {
+    event.preventDefault(); // stops the page from refreshing on submit
+    alert("data in console");
+    console.log(formData);
+  };
 
   return (
     <div>
       <Link to="/">Back / Home</Link>
       <h1>Forms</h1>
-      <form style={{display: "flex", flexDirection: "column", width: "300px"}}>
+      <form
+        onSubmit={handleSubmit}
+        style={{display: "flex", flexDirection: "column", width: "300px"}}
+      >
         <label>First Name: </label>
         <input
           type="text"
@@ -37,7 +45,6 @@ const Forms = () => {
           onChange={handleChange}
           value={first}
         />
-
         <label>Last Name: </label>
         <input
           type="text"
@@ -46,7 +53,6 @@ const Forms = () => {
           onChange={handleChange}
           value={last}
         />
-
         <label>About: </label>
         <textarea
           placeholder="About me..."
@@ -54,7 +60,6 @@ const Forms = () => {
           onChange={handleChange}
           value={about}
         />
-
         <input
           id="isFriendly"
           type="checkbox"
@@ -62,11 +67,8 @@ const Forms = () => {
           onChange={handleChange}
           checked={friendly}
         />
-
         <label htmlFor="isFriendly">Is friendly?</label>
-
         <h2>Employment Status</h2>
-
         <label>Unemployed</label>
         <input
           name="employment"
@@ -91,6 +93,15 @@ const Forms = () => {
           checked={employment === "full-time"}
           onChange={handleChange}
         />
+        <br />
+        <h3>Fave Colour</h3>
+        <select value={faveColour} onChange={handleChange} name="faveColour">
+          <option value="">---Choose---</option>
+          <option value="pink">Pink</option>
+          <option value="not pink">Not pink?</option>
+        </select>
+        {/* defaults to a submit button as it is within a form element*/}
+        <button>Submit</button>
       </form>
     </div>
   );
