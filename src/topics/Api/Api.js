7e5aaf9373ef7b64c4077bf368d0d92df13f219b
Sam <sammccathie2@gmail.com>
2022-04-12 14:36:08 +1200

    Added async option to api

--- src/topics/Api/Api.js
+++ src/topics/Api/Api.js
@@ -9,19 +9,34 @@ const Api = () => {
   const [count, setCount] = useState(1);
   const [loading, setLoading] = useState(); // added loading state
 
+  // Using async
   useEffect(() => {
     setLoading(true);
-    fetch(`https://swapi.dev/api/people/${count}`)
-      .then((res) => res.json())
-      .then((data) => setStarWarsData(data))
-      .then(() => setLoading(false));
+    async function getData() {
+      const res = await fetch(`https://swapi.dev/api/people/${count}`);
+      const data = await res.json();
+      setStarWarsData(data);
+      setLoading(false);
+    }
+    getData();
   }, [count]);
+
+  // Using .then
+  //   useEffect(() => {
+  //     setLoading(true);
+  //     fetch(`https://swapi.dev/api/people/${count}`)
+  //       .then((res) => res.json())
+  //       .then((data) => setStarWarsData(data))
+  //       .then(() => setLoading(false));
+  //   }, [count]);
 
   return (
     <div>
       <Link to="/">Back / Home</Link>
       <h1>Api</h1>
-      <button onClick={() => setCount((x) => x + 1)}>Get Next Character</button>
+      <button onClick={() => setCount((currentCount) => currentCount + 1)}>
+        Get Next Character
+      </button>
       {loading ? (
         <p>Loading...</p>
       ) : (
